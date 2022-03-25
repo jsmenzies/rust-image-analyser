8b02503bd2da9dc2ee31f86dd89ee43f935acb55
Zij-IT <elijah.reed@hartvigsen.xyz>
2022-03-25 19:59:33 +0100

    Main: Added alternative to main

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,32 @@ mod images;
 mod location;
 mod metadata;
 
+// fn main() {
+//     let mut location = images::add_location("/Users/james/proc");
+//
+//     location = images::shallow_pass_location(location);
+//
+//     location
+//         .metadata
+//         .iter()
+//         .filter(|meta| !meta.errors.is_empty())
+//         .for_each(|metadata| {
+//             println!("{:?}", metadata);
+//         });
+//
+//     // println!("{:?}", &location);
+// }
+
 fn main() {
-    let mut location = images::add_location("/Users/james/proc");
+    let location =
+        location::Location::new("/Users/james/proc").map(location::shallow_pass_location);
 
-    location = images::shallow_pass_location(location);
-
-    location
-        .metadata
-        .iter()
-        .filter(|meta| !meta.errors.is_empty())
-        .for_each(|metadata| {
-            println!("{:?}", metadata);
-        });
-
-    // println!("{:?}", &location);
+    match location {
+        Ok(loc) => {
+            for meta in loc.metadata() {
+                println!("{:?}", meta);
+            }
+        }
+        Err(errors) => println!("{:?}", errors),
+    }
 }
